81dd921767875df755452280ab2008d275e5c8ef
Camilo Quinones <camilo.quinones@cienet.com>
2026-09-06 07:21:38 +0000

    Fix steps Envy-Switch-Large to  save_every_n_steps=20 to test ScaleUp and ScaleDwon functionality

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -1155,7 +1155,7 @@ def get_trainer_kwargs(
                         config_modifiers=[
                             MeshShapeModifier.default_config().set(
                                 mesh_shape=HybridMeshShape(
-                                    ici_mesh_shape=mesh_shape_from_axes(fsdp=512),
+                                    ici_mesh_shape=mesh_shape_from_axes(fsdp=1024),
                                     dcn_mesh_shape=mesh_shape_from_axes(pipeline=1, data=2),
                                 )
                             ),
